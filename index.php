c3104854cb6d15878f7398413ba61f596307ef1b
PriyaWebDesigner <naskartanu@111gmail.com>
2024-06-28 20:39:27 +0530

    OPP

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+//Example -1
 class Car{
 
     public function color(){
@@ -20,4 +21,61 @@ $carObject = new Car ();
 $carObject->color();
 $carObject->model();
 $carObject->engine();
+
+echo '<br>'
+
 ?>
+
+<?php 
+//Example-2
+
+class Calculation{
+
+    public function sum($num1, $num2){
+        $x = $num1;
+        $y = $num2;
+        $result = $x+$y;
+
+        return $result;
+    }
+
+    public function mul(){
+        $x = 100;
+        $y = 300;
+        $result = $x*$y;
+
+        return $result;
+    }
+
+    public function sub(){
+        $x = 100;
+        $y = 300;
+        $result = $x-$y;
+
+        echo $result.'<br>';
+    }
+
+    public function div(){
+        $x = 100;
+        $y = 300;
+        $result = $x/$y;
+
+        echo $result.'<br>';
+    }
+}
+
+$calculationObject = new Calculation();
+
+echo $calculationObject -> sum(400, 400).'<br>'; //800
+
+$resultData = $calculationObject -> mul();
+echo $resultData+100;
+echo '<br>';
+
+$calculationObject -> sub();
+$calculationObject -> div();
+
+?>
+
+
+
